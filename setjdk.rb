cedebfcde27a73c9dd7137e74f1e2a21224f80d6
itshan <slamhan1987@gmail.com>
2022-03-18 15:29:46 +0800

    Update setjdk.rb

--- setjdk.rb
+++ setjdk.rb
@@ -5,7 +5,7 @@ class Setjdk < Formula
   desc "Tool for switch jdk version"
   homepage "https://github.com/MadeInChina/setjdk"
   url "https://github.com/MadeInChina/setjdk/archive/refs/tags/v1.0.0.zip"
-  sha256 "71aaa07f3dffe04a927e3cab72072d8fe00b6b54111359ef0c15d33a8bfdb2c1"
+  sha256 "3eb0407ab6b1a99e70fb52a9feb9edbced88c04800613e042693201358469fef"
   license "Apache-2.0"
 
   def install
